73b1771d853677745de5c27403f802e65eaa14c5
Ionatan Dumea <dumeaionatan9@gmail.com>
2025-09-16 09:42:47 +0300

    chore: spinup the config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InvoicesModule } from './invoices/invoices.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [InvoicesModule, AuthModule],
+  imports: [
+    InvoicesModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
