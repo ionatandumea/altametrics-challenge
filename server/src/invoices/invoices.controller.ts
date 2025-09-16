bcf5fab6eaffa268df99a01e49c0fb4723d38543
Ionatan Dumea <dumeaionatan9@gmail.com>
2025-09-16 19:54:20 +0300

    chore(server): remove public decorator from route

--- server/src/invoices/invoices.controller.ts
+++ server/src/invoices/invoices.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { InvoicesService } from 'src/invoices/invoices.service';
-import { Public } from 'src/decorators/public.decorator';
 
 @Controller('invoices')
 export class InvoicesController {
   constructor(private readonly invoicesService: InvoicesService) {}
 
-  @Public()
   @Get()
   findAll() {
     return this.invoicesService.findAll();
